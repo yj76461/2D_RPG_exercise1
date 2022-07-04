77c320b808211648515ba9b8c6006907a574516b
yunju desktop <yj76461@gmail.com>
2022-07-04 18:29:01 +0900

    third lecture

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MovingObject : MonoBehaviour
 {
+    private BoxCollider2D boxCollider;
+    public LayerMask layerMask;
     public float speed;
     private Vector3 vector;
     public float runSpeed;
@@ -19,6 +21,7 @@ public class MovingObject : MonoBehaviour
 
     // Start is called before the first frame update
     void Start(){
+        boxCollider = GetComponent<BoxCollider2D>();
         animator = GetComponent<Animator>();
     }
 
@@ -42,6 +45,21 @@ public class MovingObject : MonoBehaviour
             
             animator.SetFloat("DirX", vector.x);
             animator.SetFloat("DirY", vector.y);
+
+            RaycastHit2D hit;
+
+            Vector2 start = transform.position; // location of character
+            Vector2 end = start + new Vector2(vector.x * speed * walkCount,
+            vector.y * speed * walkCount); // location of destination
+
+            boxCollider.enabled = false;
+            hit = Physics2D.Linecast(start, end, layerMask);
+            boxCollider.enabled = true;
+
+            if(hit.transform != null)
+                break;
+            
+
             animator.SetBool("Walking", true);
 
             while(currentWalkCount < walkCount){

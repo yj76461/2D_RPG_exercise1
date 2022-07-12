d0ae8fec3bb7b98d64f8c7db75f6552fd9699537
yunju desktop <yj76461@gmail.com>
2022-07-12 13:30:01 +0900

    5th lecture

---
+++ Assets/SuperTiled2Unity/import/tileSetA.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="Forest_A5" tilewidth="48" tileheight="48" tilecount="128" columns="8">
+ <image source="../../Tiled/Forest_A5.png" width="384" height="768"/>
+</tileset>
